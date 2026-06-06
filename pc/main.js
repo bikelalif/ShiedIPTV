7e42c4c97d93f798432211719b5604dea5783161
Bilal <bilal@example.com>
2026-06-06 19:54:22 +0200

    feat: ajout de la version PC (Electron) avec ecran d'acceptation des CGU

---
+++ pc/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        minWidth: 1024,
+        minHeight: 576,
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            webSecurity: false // Disables CORS checks so local app can query IPTV HTTP streams directly
+        }
+    });
+
+    // Load local web files from the copy in pc/src/
+    win.loadFile(path.join(__dirname, 'src/index.html'));
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
