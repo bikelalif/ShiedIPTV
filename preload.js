9b363457e2933792f756590536bd941ed4331210
Bilal <bilal@example.com>
2026-06-22 23:30:58 +0200

    chore: commit DoH improvements, cleanup unused JSON scripts, and sync client files

--- preload.js
+++ preload.js
@@ -6,5 +6,14 @@ contextBridge.exposeInMainWorld('electronAPI', {
     resizeVlc: (rect) => ipcRenderer.invoke('resize-vlc', rect),
     undockVlc: () => ipcRenderer.invoke('undock-vlc'),
     onVlcExited: (callback) => ipcRenderer.on('vlc-exited', () => callback()),
+
+    // Embedded native player (mpv / VLC)
+    playNative: (engine, url, rect) => ipcRenderer.invoke('play-native', engine, url, rect),
+    updateNativeRect: (rect) => ipcRenderer.invoke('update-native-rect', rect),
+    stopNative: () => ipcRenderer.invoke('stop-native'),
+    nativeCommand: (cmd) => ipcRenderer.invoke('native-command', cmd),
+    onNativeExited: (callback) => ipcRenderer.on('native-exited', () => callback()),
+    onNativeError: (callback) => ipcRenderer.on('native-error', (e, code) => callback(code)),
+
     isElectron: true
 });
